5bc6d911c8794b6b2cda29239554fbd0bbaf392e
Charles Yiu <me@charr.cc>
2022-06-08 15:23:35 +0800

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,9 @@
 declare module "instance-decorators" {
     export type InstanceDecorator = (_target: any, _name: string, _descriptor?: PropertyDescriptor) => void
-    export type InstanceAction<Thing> = ((target: Thing, propertyKey: string, propertyDescriptor: PropertyDescriptor) => any) | ((target: Thing, propertyKey: string) => any)
+    export type InstanceAction = ((target: any, propertyKey: string, propertyDescriptor: PropertyDescriptor) => any) | ((target: Thing, propertyKey: string) => any)
     
-    export function Instance<Thing>(): (<Thing>(target: any) => any)
-    export function Instance<Thing>(action: InstanceAction<Thing>): InstanceDecorator
-    export function Instance<Thing>(target?: any, name?: string, descriptor?: PropertyDescriptor): (((target: any) => any) | InstanceDecorator | void)
+    export function Instance(): ((target: any) => any)
+    export function Instance(action: InstanceAction): InstanceDecorator
+    export function Instance(target?: any, name?: string, descriptor?: PropertyDescriptor): (((target: any) => any) | InstanceDecorator | void)
     export default Instance
 }
